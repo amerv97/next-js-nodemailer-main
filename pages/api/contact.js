68df12c4bf4ffb9d2c8ac9a2ef64ad8829c62c61
Amer Varajic <varajicamer@gmail.com>
2023-06-06 15:33:22 +0200

    converted in arrow funkction

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  console.log(req.body)
-  res.status(200).json({ name: 'John Doe' })
-}
+const handler = (req, res) => {
+  console.log(req.body);
+  res.status(200).json({ name: "John Doe" });
+};
+
+export default handler;
