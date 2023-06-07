3d1ef35dad15221c4e0f628feb0a9aacca0502f3
Amer Varajic <varajicamer@gmail.com>
2023-06-07 11:57:28 +0200

    created nodemailer & backend

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,8 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const handler = (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ name: "John Doe" });
+import { mailOptions, transporter } from "../../config/nodemailer";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+    if (!data.name || !data.email || !data.subject || !data.message) {
+      return res.status(400).json({ message: "Bad request" });
+    }
+
+    try {
+      await transporter.sendMail({
+        ...mailOptions,
+        subject: data.subject,
+        text: "This is a test string",
+        html: "<h1>Test Title</h1><p>Some body text</p>",
+      });
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: error.message });
+    }
+  }
 };
 
 export default handler;
